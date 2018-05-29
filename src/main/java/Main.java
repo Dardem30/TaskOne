cf634611d7baea79b4dce9e417b84623c729809f
unknown <r.lukashenko@mail.ru>
2018-05-29 23:09:16 +0300

    correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,14 @@ import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.util.PDFMergerUtility;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Properties properties=new Properties();
-        properties.load(new FileInputStream("C:\\Users\\Roman\\SecondTask\\src\\main\\java\\file.properties"));
+        FileInputStream fileInputStream=new FileInputStream("file.properties");
+        properties.load(fileInputStream);
         PDFMergerUtility ut = new PDFMergerUtility();
         ut.addSource(properties.getProperty("firstPath"));
         ut.addSource(properties.getProperty("secondPath"));
